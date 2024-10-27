30afb39bad38a0e3dc1b18850c674a32355d04c4
notnatdf <myec1234@gmail.com>
2024-10-27 18:40:46 +0900

    home/increase request

--- src/main/java/com/sbs/basic1/controller/HomeController.java
+++ src/main/java/com/sbs/basic1/controller/HomeController.java
@@ -6,9 +6,34 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    int count ;
+
+    public HomeController () {
+        count = -1;
+    }
+
     @GetMapping("/home/main")
     @ResponseBody
     public String showHome() {
         return "안녕하세요";
+    }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showHome2() {
+        return "환영합니다.";
+    }
+
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showHome3() {
+        return "Spring Boot Rulls!!.";
+    }
+
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public int showIncrease() {
+        count++;
+        return count;
     }
 }
